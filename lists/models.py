373449db39ba455147128a42732b2eea61cb1d04
YungLeChao <l-zhaoyl@qq.com>
2016-02-15 13:33:18 +0800

    Model change

--- lists/models.py
+++ lists/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
 
+class List(models.Model):
+    pass
+
+
 class Item(models.Model):
     """docstring for Item"""
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None)
+
 
 # Create your models here.
