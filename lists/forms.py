1431f6de76f1321d6cb41c91f2d0169cff80a8e8
yunglechao <l-zhaoyl@qq.com>
2016-02-20 15:55:21 +0800

    new form for list items

---
+++ lists/forms.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: YungLeChao
+# @Date:   2016-02-20 15:13:05
+# @Last Modified by:   YungLeChao
+# @Last Modified time: 2016-02-20 15:54:49
+# @Email: zhaoyongle77@gmail.com
+
+from __future__ import print_function
+from django import forms
+from lists.models import Item
+
+EMPTY_LIST_ERROR = "You can't have an empty list item"
+
+
+class ItemForm(forms.models.ModelForm):
+    """docstring for ItemForm"""
+    class Meta:
+        model = Item
+        fields = ('text',)
+        widgets = {'text': forms.fields.TextInput(
+            attrs={'placeholder': 'Enter a to-do item', 'class': 'form-control input-lg', }), }
+        error_messages = {
+            'text': {'required': EMPTY_LIST_ERROR}}
---
+++ lists/tests/test_forms.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: YungLeChao
+# @Date:   2016-02-20 15:09:19
+# @Last Modified by:   YungLeChao
+# @Last Modified time: 2016-02-20 15:54:39
+# @Email: zhaoyongle77@gmail.com
+
+from __future__ import print_function
+from django.test import TestCase
+
+from lists.forms import EMPTY_LIST_ERROR, ItemForm
+
+
+class ItemFormTest(TestCase):
+    def test_form_renders_item_input_has_placeholder_and_css_classes(self):
+        form = ItemForm()
+        self.assertIn('placeholder="Enter a to-do item"', form.as_p())
+        self.assertIn('class="form-control input-lg"', form.as_p())
+
+    def test_form_validation_for_blank_items(self):
+        form = ItemForm(data={'text': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(
+            form.errors['text'], [EMPTY_LIST_ERROR])
