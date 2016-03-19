53e6df14daa6cf72ba23fb7db9de524a64fcca1a
yunglechao <l-zhaoyl@qq.com>
2016-03-19 17:14:29 +0800

    start app accounts

---
+++ superlists/static/list.js
@@ -0,0 +1,12 @@
+/*
+* @Author: YungLeChao
+* @Date:   2016-03-18 22:12:12
+* @Last Modified by:   YungLeChao
+* @Last Modified time: 2016-03-18 22:24:20
+*/
+jQuery(document).ready(function ($) {
+    $('input').on('keypress', function () {
+        $('.has-error').hide();
+    });
+});
+
